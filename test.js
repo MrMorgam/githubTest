70e2c7ecf54b3987899e2f3a3bf0cd4a8b548f8b
Gabriel Morais <gabriel.am.mor@gmail.com>
2022-05-30 12:05:54 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-import { prompt } from 'prompt-sync'
-const input = prompt(signit: true)
+import prompt from 'prompt-sync'
+const input = prompt()
 
 function main() {
     console.log('This is a test')
